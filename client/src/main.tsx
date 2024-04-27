f0bb175534cb2c4bcab59754ae495ceab42a564b
Kelechi <kelechi@utexas.edu>
2024-04-27 07:34:43 +0000

    minor styling changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import '@mantine/core/styles.css';
 import './index.css'
+import { MantineProvider, createTheme } from '@mantine/core';
+
+const theme  = createTheme({
+  primaryShade: {light: 5, dark: 8},
+  primaryColor: 'orange',
+  fontFamily: 'Roboto',
+  headings: {
+    fontFamily: 'Roboto',
+  },
+  components: { "badge" :{
+    
+  } },
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
